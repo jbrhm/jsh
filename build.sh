0424f0943b24e3e3b2e6f4575991dc3fd864d062
jbrhm <jrabraham1904@gmail.com>
2025-04-29 23:31:43 -0400

    added functionality for mold

--- build.sh
+++ build.sh
@@ -5,6 +5,9 @@ set -Eeuo pipefail
 if test -d build; then
     echo "Build Directory Exists..."
 else
-    cmake . -B build -G Ninja
+    cmake \
+        -DCMAKE_CXX_COMPILER=$(which clang++) \
+        -DCMAKE_C_COMPILER=$(which clang) \
+        . -B build -G Ninja
 fi
 cmake --build build
