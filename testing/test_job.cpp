faf3fa8a6bfdb2cf4bdc56b683d5ca0ee5889261
jbrhm <jrabraham1904@gmail.com>
2025-06-08 12:10:14 -0400

    passing edge cases

--- testing/test_job.cpp
+++ testing/test_job.cpp
@@ -4,6 +4,21 @@
 // JSH
 #include <job.hpp>
 
+TEST(TestJob, TestParseJobNoOperators){
+    // input
+    std::string input = "echo hi";
+
+    auto j = jsh::job::parse_job(input);
+
+    // ensure correct sizing
+    ASSERT_TRUE(j->process_seq.size() == 1);
+    ASSERT_TRUE(j->input_seq.size() == 1);
+    ASSERT_TRUE(j->operator_seq.size() == 0);
+
+    // ensure correct contents for input and operator sequence
+    ASSERT_STREQ(j->input_seq[0].c_str(), "echo hi");
+}
+
 TEST(TestJob, TestParseJobAndBasic1){
     // input
     std::string input = "echo hi && echo hi";
@@ -18,5 +33,145 @@ TEST(TestJob, TestParseJobAndBasic1){
     // ensure correct contents for input and operator sequence
     ASSERT_STREQ(j->input_seq[0].c_str(), "echo hi ");
     ASSERT_STREQ(j->input_seq[1].c_str(), " echo hi");
+    ASSERT_EQ(j->operator_seq[0], jsh::job::OPERATOR::AND);
+}
+
+TEST(TestJob, TestParseJobAndBasic2){
+    // input
+    std::string input = "echo hi&& echo hi";
+
+    auto j = jsh::job::parse_job(input);
+
+    // ensure correct sizing
+    ASSERT_TRUE(j->process_seq.size() == 2);
+    ASSERT_TRUE(j->input_seq.size() == 2);
+    ASSERT_TRUE(j->operator_seq.size() == 1);
+
+    // ensure correct contents for input and operator sequence
+    ASSERT_STREQ(j->input_seq[0].c_str(), "echo hi");
+    ASSERT_STREQ(j->input_seq[1].c_str(), " echo hi");
+    ASSERT_EQ(j->operator_seq[0], jsh::job::OPERATOR::AND);
+}
+
+TEST(TestJob, TestParseJobAndBasic3){
+    // input
+    std::string input = "echo hi &&echo hi";
+
+    auto j = jsh::job::parse_job(input);
+
+    // ensure correct sizing
+    ASSERT_TRUE(j->process_seq.size() == 2);
+    ASSERT_TRUE(j->input_seq.size() == 2);
+    ASSERT_TRUE(j->operator_seq.size() == 1);
+
+    // ensure correct contents for input and operator sequence
+    ASSERT_STREQ(j->input_seq[0].c_str(), "echo hi ");
+    ASSERT_STREQ(j->input_seq[1].c_str(), "echo hi");
+    ASSERT_EQ(j->operator_seq[0], jsh::job::OPERATOR::AND);
+}
+
+TEST(TestJob, TestParseJobAndBasic4){
+    // input
+    std::string input = "echo hi&&echo hi";
+
+    auto j = jsh::job::parse_job(input);
+
+    // ensure correct sizing
+    ASSERT_TRUE(j->process_seq.size() == 2);
+    ASSERT_TRUE(j->input_seq.size() == 2);
+    ASSERT_TRUE(j->operator_seq.size() == 1);
+
+    // ensure correct contents for input and operator sequence
+    ASSERT_STREQ(j->input_seq[0].c_str(), "echo hi");
+    ASSERT_STREQ(j->input_seq[1].c_str(), "echo hi");
+    ASSERT_EQ(j->operator_seq[0], jsh::job::OPERATOR::AND);
+}
+
+TEST(TestJob, TestParseJobAndEdge1){
+    // input
+    std::string input = "&&";
+
+    auto j = jsh::job::parse_job(input);
+
+    // ensure correct sizing
+    ASSERT_TRUE(j->process_seq.size() == 2);
+    ASSERT_TRUE(j->input_seq.size() == 2);
+    ASSERT_TRUE(j->operator_seq.size() == 1);
+
+    // ensure correct contents for input and operator sequence
+    ASSERT_STREQ(j->input_seq[0].c_str(), "");
+    ASSERT_STREQ(j->input_seq[1].c_str(), "");
+    ASSERT_EQ(j->operator_seq[0], jsh::job::OPERATOR::AND);
+}
+
+TEST(TestJob, TestParseJobAndEdge2){
+    // input
+    std::string input = "&&&";
+
+    auto j = jsh::job::parse_job(input);
+
+    // ensure correct sizing
+    ASSERT_TRUE(j->process_seq.size() == 2);
+    ASSERT_TRUE(j->input_seq.size() == 2);
+    ASSERT_TRUE(j->operator_seq.size() == 1);
+
+    // ensure correct contents for input and operator sequence
+    ASSERT_STREQ(j->input_seq[0].c_str(), "");
+    ASSERT_STREQ(j->input_seq[1].c_str(), "&");
+    ASSERT_EQ(j->operator_seq[0], jsh::job::OPERATOR::AND);
+}
+
+TEST(TestJob, TestParseJobAndEdge3){
+    // input
+    std::string input = "&&&&";
+
+    auto j = jsh::job::parse_job(input);
+
+    // ensure correct sizing
+    ASSERT_TRUE(j->process_seq.size() == 3);
+    ASSERT_TRUE(j->input_seq.size() == 3);
+    ASSERT_TRUE(j->operator_seq.size() == 2);
+
+    // ensure correct contents for input and operator sequence
+    ASSERT_STREQ(j->input_seq[0].c_str(), "");
+    ASSERT_STREQ(j->input_seq[1].c_str(), "");
+    ASSERT_STREQ(j->input_seq[2].c_str(), "");
+    ASSERT_EQ(j->operator_seq[0], jsh::job::OPERATOR::AND);
+    ASSERT_EQ(j->operator_seq[1], jsh::job::OPERATOR::AND);
+}
+
+TEST(TestJob, TestParseJobAndEdge4){
+    // input
+    std::string input = "&&a&& command";
+
+    auto j = jsh::job::parse_job(input);
+
+    // ensure correct sizing
+    ASSERT_TRUE(j->process_seq.size() == 3);
+    ASSERT_TRUE(j->input_seq.size() == 3);
+    ASSERT_TRUE(j->operator_seq.size() == 2);
+
+    // ensure correct contents for input and operator sequence
+    ASSERT_STREQ(j->input_seq[0].c_str(), "");
+    ASSERT_STREQ(j->input_seq[1].c_str(), "a");
+    ASSERT_STREQ(j->input_seq[2].c_str(), " command");
+    ASSERT_EQ(j->operator_seq[0], jsh::job::OPERATOR::AND);
+    ASSERT_EQ(j->operator_seq[1], jsh::job::OPERATOR::AND);
+}
+
+TEST(TestJob, TestParseJobAndEdge5){
+    // input
+    std::string input = "aa&& command";
+
+    auto j = jsh::job::parse_job(input);
+
+    // ensure correct sizing
+    ASSERT_TRUE(j->process_seq.size() == 2);
+    ASSERT_TRUE(j->input_seq.size() == 2);
+    ASSERT_TRUE(j->operator_seq.size() == 1);
+
+    // ensure correct contents for input and operator sequence
+    ASSERT_STREQ(j->input_seq[0].c_str(), "aa");
+    ASSERT_STREQ(j->input_seq[1].c_str(), " command");
     ASSERT_EQ(j->operator_seq[0], jsh::job::OPERATOR::AND);
 }
