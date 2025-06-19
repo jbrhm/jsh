7169469e4ba24db9d2ede2c1161a8893cf8f2f47
jbrhm <jrabraham1904@gmail.com>
2025-06-18 20:55:14 -0400

    redo the stdin to pipe copying

--- testing/utils/write_pipe.cpp
+++ testing/utils/write_pipe.cpp
@@ -1,7 +1,9 @@
 // UNIX
+#include <unistd.h>
 #include <getopt.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <fcntl.h>
 
 // STD
 #include <cassert>
@@ -50,10 +52,51 @@ int main(int argc, char** argv){
     // create the pipe
     if(mkfifo(pipe_name.c_str(), perms) == -1){
         std::cout << "Error making pipe: " << strerror(errno) << '\n';
+        return 1;
     }
 
     // open the pipe and write to it
-    int fd = open()
+    int fd = open(pipe_name.c_str(), O_CREAT | O_RDWR);
+    if(fd == -1){
+        std::cout << "Error opening pipe: " << strerror(errno) << '\n';
+        return 1;
+    }
+
+    // read in from stdin
+    // TODO: REDO this portion
+    assert(false);
+    std::string str;
+    while(std::cin >> str){
+        int num_bytes_written = 0;
+
+        // write the string to the pipe
+        while(num_bytes_written != str.size()){
+            ssize_t num_written = write(fd, str.c_str(), str.size());
+
+            if(num_written == -1){
+                std::cout << "Error writing to pipe: " << strerror(errno) << '\n';
+                return 1;
+            }
+
+            num_bytes_written += static_cast<int>(num_written);
+        }
+
+        int num_bytes_read = 0;
+
+        // write the string to the pipe
+        while(num_bytes_read != str.size()){
+            ssize_t num_read = read(fd, str.data(), str.size());
+
+            if(num_read == -1){
+                std::cout << "Error reading from pipe: " << strerror(errno) << '\n';
+                return 1;
+            }
+
+            num_bytes_read += static_cast<int>(num_read);
+        }
+
+        std::cout << str << '\n';
+    }
 
     return 0;
 }
