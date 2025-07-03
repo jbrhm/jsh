612975ae44edde2d60f450015a10ca7208002662
John <jrabraham1904@gmail.com>
2025-07-03 11:47:31 -0400

    fixing linker issues

--- build.sh
+++ build.sh
@@ -9,8 +9,6 @@ else
         -DCMAKE_BUILD_TYPE=Debug \
         -DCMAKE_CXX_COMPILER=$(which clang++) \
         -DCMAKE_C_COMPILER=$(which clang) \
-        -DCMAKE_CXX_FLAGS="-stdlib=libc++ -fno-rtti" \
-        -DCMAKE_C_FLAGS="-stdlib=libc++" \
         . -B build -G Ninja
 fi
 cmake --build build
