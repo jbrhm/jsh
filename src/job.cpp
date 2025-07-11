12ea89ace1933574f1b929d29823887ae17960db
jbrhm <jrabraham1904@gmail.com>
2025-07-10 22:26:42 -0400

    fixed && operator

--- src/job.cpp
+++ src/job.cpp
@@ -107,36 +107,41 @@ namespace jsh {
         // perform the process' execution
         assert(data->input_seq.size() == data->process_seq.size());
         assert(data->input_seq.size() == data->operator_seq.size() + 1);
-        for(std::size_t i = 0; i < data->process_seq.size(); ++i){
+        for(std::size_t i = 0; i + 1 < data->process_seq.size(); ++i){
             // get a reference to the process_data
             std::unique_ptr<process_data>& proc_data = data->process_seq[i];
 
-            // if on last command dont do any checks
-            if(i + 1 != data->process_seq.size()){
-                // we should not have any invalid operators at this point
-                assert(data->operator_seq[i] < OPERATOR::COUNT);
+            // we should not have any invalid operators at this point
+            assert(data->operator_seq[i] < OPERATOR::COUNT);
 
-                // Check if the current process' output is being piped somewhere else
-                if(data->operator_seq[i] == OPERATOR::PIPE){
-                    // create the pipe
-                    std::optional<std::vector<file_descriptor_wrapper>> pipe_fds_op = syscall_wrapper::pipe_wrapper();
+            // Check if the current process' output is being piped somewhere else
+            if(data->operator_seq[i] == OPERATOR::PIPE){
+                // create the pipe
+                std::optional<std::vector<file_descriptor_wrapper>> pipe_fds_op = syscall_wrapper::pipe_wrapper();
 
-                    // error handle
-                    if(!pipe_fds_op.has_value()){
-                        return;
-                    }
+                // error handle
+                if(!pipe_fds_op.has_value()){
+                    return;
+                }
 
-                    // get pipe fds
-                    assert(pipe_fds_op.has_value());
-                    assert(pipe_fds_op.value().size() == 2);
-                    std::vector<file_descriptor_wrapper> pipe_fds = std::move(pipe_fds_op.value());
-                    assert(pipe_fds.size() == 2);
+                // get pipe fds
+                assert(pipe_fds_op.has_value());
+                assert(pipe_fds_op.value().size() == 2);
+                std::vector<file_descriptor_wrapper> pipe_fds = std::move(pipe_fds_op.value());
+                assert(pipe_fds.size() == 2);
 
-                    // set the current output and the next input to read from the pipe
-                    std::visit([&](auto&& var){var.stdout = std::move(pipe_fds[1]);}, *proc_data);
-                    // there will always be another process since this is being piped somewhere else
-                    assert(data->process_seq.size() > i);
-                    std::visit([&](auto&& var){var.stdin = std::move(pipe_fds[0]);}, *data->process_seq[i+1]);
+                // set the current output and the next input to read from the pipe
+                std::visit([&](auto&& var){var.stdout = std::move(pipe_fds[1]);}, *proc_data);
+                // there will always be another process since this is being piped somewhere else
+                assert(data->process_seq.size() > i);
+                std::visit([&](auto&& var){var.stdin = std::move(pipe_fds[0]);}, *data->process_seq[i+1]);
+            }else if(data->operator_seq[i] == OPERATOR::AND){
+                // get the exit status from previous process
+                std::string exit_status = environment::get_var(environment::STATUS_STRING);
+
+                // check to see if it was not sucessful, if so break out of the loop
+                if(exit_status != environment::SUCCESS_STRING){
+                    break;
                 }
             }
             
